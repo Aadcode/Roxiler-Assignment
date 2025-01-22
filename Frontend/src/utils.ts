d2cc4c9928b778075a72f21fbe47055c293d3b4b
Aadcode <118838917+Aadcode@users.noreply.github.com>
2025-01-22 12:52:54 +0530

    Changes for Deployment

--- Frontend/src/utils.ts
+++ Frontend/src/utils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://roxilerdashboard-i9wp.onrender.com/api/v1",
 });
 
 export default apiClient
